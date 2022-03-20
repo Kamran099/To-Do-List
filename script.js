e22a9c4a4999140203d8760b0d04a8a5c9d5dd47
Kamran <kamran-anwar99@hotmail.com>
2022-03-20 22:32:22 +0000

    Started work on: User able to add tasks + some CSS

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("Please enter a task");
+    }
+    else {
+        document.querySelector('#tasks').innerHTML += `
+        <div class="task">
+        <span id ="taskname">
+            ${document.querySelector
+                ('#newtask input').value}
+            </span>
+            <button class="delete">
+            <i class="far fa-trash-alt"></i>
+            </button>
+        </div>
+        `;
+    }
+}
